415ff78a5157bf56f19a03d5ed8eb9216b65f8bd
Andrew Minnich <aminnich3@gmail.com>
2016-11-01 02:08:00 -0400

    Volume control

--- main.lua
+++ main.lua
@@ -5,10 +5,18 @@ local testSound = ripple.newSound 'boop.ogg'
 function love.keypressed(key)
   if key == 'p' then
     testSound:play {
+      volume = love.math.random(),
       pitch = 2,
     }
   end
   if key == 's' then testSound:stop() end
+  if key == '5' then
+    if testSound:_getVolume() == .5 then
+      testSound:_setVolume(1)
+    else
+      testSound:_setVolume(.5)
+    end
+  end
   if key == 'escape' then love.event.quit() end
 end
 
--- ripple.lua
+++ ripple.lua
@@ -2,15 +2,21 @@ local Instance = {}
 
 function Instance:_isPlaying() return self._source:isPlaying() end
 
+function Instance:_update()
+  self._source:setVolume(self._volume * self._sound:_getFinalVolume())
+end
+
 function Instance:_stop() self._source:stop() end
 
 local function newInstance(sound, options)
   options = options or {}
   local instance = {
+    _sound = sound,
     _source = sound._source:clone(),
     _volume = options.volume or 1,
   }
   setmetatable(instance, {__index = Instance})
+  instance:_update()
   instance._source:setPitch(options.pitch or 1)
   instance._source:play()
   return instance
@@ -18,6 +24,23 @@ end
 
 
 local Sound = {}
+
+function Sound:_getVolume() return self._volume end
+
+function Sound:_setVolume(volume)
+  self._volume = volume
+  self:_update()
+end
+
+function Sound:_getFinalVolume()
+  return self._volume
+end
+
+function Sound:_update()
+  for i = 1, #self._instances do
+    self._instances[i]:_update()
+  end
+end
 
 function Sound:_clean()
   for i = #self._instances, 1, -1 do
@@ -44,7 +67,7 @@ local function newSound(filename, tags)
   local sound = {
     _source = love.audio.newSource(filename),
     _tags = tags or {},
-    _volume = {},
+    _volume = 1,
     _instances = {},
   }
   setmetatable(sound, {__index = Sound})
