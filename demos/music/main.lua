a60b993fc3bb90d1cae7cc20af24c3c90ebc7a9e
Andrew Minnich <aminnich3@gmail.com>
2016-11-01 23:08:35 -0400

    Cowbell button + use layouts

--- demos/music/main.lua
+++ demos/music/main.lua
@@ -1,14 +1,20 @@
 local suit = require 'suit'
 
+-- load ripple
 local ripple = require 'ripple'
 
+-- tags
 local tags = {
   main = ripple.newTag(),
   aux = ripple.newTag(),
   master = ripple.newTag()
 }
 
+-- sounds
 local sounds = {
+  cowbell = ripple.newSound('cowbell.ogg', {
+    tags = {tags.master},
+  }),
   introMain = ripple.newSound('intro main.ogg', {
     bpm = 95,
     length = '2b',
@@ -30,14 +36,17 @@ local sounds = {
     tags = {tags.aux, tags.master},
   }),
 }
+-- start loop after intro
 sounds.introMain.onEnd = function()
   sounds.loopMain:play()
   sounds.loopAux:play()
 end
+-- looping behavior
 sounds.loopMain.onEnd = function()
   sounds.loopMain:play()
   sounds.loopAux:play()
 end
+-- make the squares flash
 sounds.introMain.every['1m'] = function() flashingBoxes[1].alpha = 255 end
 sounds.introMain.every['1b'] = function() flashingBoxes[2].alpha = 255 end
 sounds.introMain.every['.5b'] = function() flashingBoxes[3].alpha = 255 end
@@ -45,28 +54,46 @@ sounds.loopMain.every['1m'] = function() flashingBoxes[1].alpha = 255 end
 sounds.loopMain.every['1b'] = function() flashingBoxes[2].alpha = 255 end
 sounds.loopMain.every['.5b'] = function() flashingBoxes[3].alpha = 255 end
 
+-- the rest is just the code that makes the demo work
 local mainSlider = {value = 1, min = 0, max = 1}
 local auxSlider = {value = 1, min = 0, max = 1}
 local masterSlider = {value = 1, min = 0, max = 1}
 flashingBoxes = {{alpha = 0}, {alpha = 0}, {alpha = 0}}
 
 function love.update(dt)
-  if suit.Button('Play', 50, 50, 300, 50).hit then
+  suit.layout:reset(50, 50, 25)
+
+  suit.layout:push(suit.layout:row(700, 50))
+  if suit.Button('Play', suit.layout:col(650/3, 50)).hit then
     sounds.introMain:play()
     sounds.introAux:play()
   end
-  if suit.Button('Stop', 450, 50, 300, 50).hit then
+  if suit.Button('Cowbell', suit.layout:col()).hit then
+    sounds.cowbell:play {
+      volume = love.math.random()*.5 + .5,
+      pitch = love.math.random()*.5 + .5,
+    }
+  end
+  if suit.Button('Stop', suit.layout:col()).hit then
     for _, sound in pairs(sounds) do
       sound:stop()
     end
   end
+  suit.layout:pop()
 
-  suit.Label('Kick + Snare', 50, 150, 100, 20)
-  suit.Slider(mainSlider, 200, 150, 550, 20)
-  suit.Label('Aux percussion', 50, 190, 100, 20)
-  suit.Slider(auxSlider, 200, 190, 550, 20)
-  suit.Label('Master', 50, 230, 100, 20)
-  suit.Slider(masterSlider, 200, 230, 550, 20)
+  suit.layout:push(suit.layout:row(700, 20))
+  suit.Label('Kick + snare', suit.layout:col(100, 20))
+  suit.Slider(mainSlider, suit.layout:col(575, 20))
+  suit.layout:pop()
+  suit.layout:push(suit.layout:row(700, 20))
+  suit.Label('Aux percussion', suit.layout:col(100, 20))
+  suit.Slider(auxSlider, suit.layout:col(575, 20))
+  suit.layout:pop()
+  suit.layout:push(suit.layout:row(700, 20))
+  suit.Label('Master', suit.layout:col(100, 20))
+  suit.Slider(masterSlider, suit.layout:col(575, 20))
+  suit.layout:pop()
+
   tags.main.volume.v = mainSlider.value
   tags.aux.volume.v = auxSlider.value
   tags.master.volume.v = masterSlider.value
@@ -78,6 +105,7 @@ function love.update(dt)
     end
   end
 
+  -- update all sounds
   for _, sound in pairs(sounds) do
     sound:update(dt)
   end
@@ -87,12 +115,11 @@ function love.keypressed(key)
   if key == 'escape' then love.event.quit() end
 end
 
-
 function love.draw()
   suit.draw()
+  suit.layout:push(suit.layout:row(700, 75))
   for i = 1, #flashingBoxes do
-    local x = 50 + 100 * (i - 1)
-    local y = 290
+    local x, y = suit.layout:col(75, 75)
     local w = 75
     local h = 75
     love.graphics.setColor(255, 255, 255)
@@ -101,4 +128,5 @@ function love.draw()
     love.graphics.setColor(255, 255, 255, flashingBoxes[i].alpha)
     love.graphics.rectangle('fill', x, y, w, h)
   end
+  suit.layout:pop()
 end
