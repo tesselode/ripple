ac56c8bf464162d877d90284a862c45ae4016492
Andrew Minnich <aminnich3@gmail.com>
2018-07-05 17:53:03 -0400

    jk you actually can stack effects

--- main.lua
+++ main.lua
@@ -1,30 +1,30 @@
 local ripple = require 'ripple'
 
-love.audio.setEffect('cavern', {
+love.audio.setEffect('reverb', {
 	type = 'reverb',
 	decaytime = 5,
 	gain = .5,
 })
 
-local sfx = ripple.newTag()
-sfx:setEffect 'cavern'
+love.audio.setEffect('chorus', {
+	type = 'chorus',
+})
+
+love.audio.setEffect('echo', {
+	type = 'echo',
+	delay = .5,
+	feedback = .5,
+})
 
 local testSound = ripple.newSound {
 	source = love.audio.newSource('bloop.ogg', 'static'),
-	tags = {sfx},
 }
-local testSound2 = ripple.newSound {
-	source = love.audio.newSource('bloop2.ogg', 'static'),
-	tags = {sfx},
-}
+testSound:setEffect 'chorus'
+testSound:setEffect 'echo'
+testSound:setEffect 'reverb'
 
 function love.keypressed(key)
 	if key == '1' then testSound:play() end
-	if key == '2' then testSound2:play() end
-
-	if key == 'return' then
-		testSound:setLooping(not testSound:isLooping())
-	end
 end
 
 function love.draw()
