a9bd4b79f21e691ab79d99901efcb490c58be441
Andrew Minnich <aminnich3@gmail.com>
2019-06-20 01:13:08 -0400

    versatile tag system
    tags, instances, and sounds can all be tagged

--- main.lua
+++ main.lua
@@ -1,11 +1,18 @@
 local ripple = require 'ripple'
 
+local testTag1 = ripple.newTag {volume = .5}
+local testTag2 = ripple.newTag {volume = .1}
+
 local testSound = ripple.newSound(love.audio.newSource('test/bloop.ogg', 'static'))
 
 function love.keypressed(key)
 	if key == 'space' then
 		instance = testSound:play()
-	elseif instance then
-		instance.volume = instance.volume * .5
+	elseif key == '1' then
+		testSound:tag(testTag1)
+	elseif key == '2' then
+		if instance then
+			instance:tag(testTag2)
+		end
 	end
 end
--- ripple.lua
+++ ripple.lua
@@ -1,9 +1,80 @@
 local ripple = {}
 
+local Tag = {}
+
+function Tag:_getTotalVolume()
+	local volume = self.volume
+	for tag, _ in pairs(self._tags) do
+		volume = volume * tag:_getTotalVolume()
+	end
+	return volume
+end
+
+function Tag:_onChangeVolume()
+	for child, _ in pairs(self._children) do
+		child:_onChangeVolume()
+	end
+end
+
+function Tag:tag(...)
+	for i = 1, select('#', ...) do
+		local tag = select(i, ...)
+		self._tags[tag] = true
+		tag._children[self] = true
+	end
+	self:_onChangeVolume()
+end
+
+function Tag:untag(...)
+	for i = 1, select('#', ...) do
+		local tag = select(i, ...)
+		self._tags[tag] = nil
+		tag._children[self] = nil
+	end
+	self:_onChangeVolume()
+end
+
+function Tag:__index(key)
+	if key == 'volume' then
+		return self._volume
+	end
+	return Tag[key]
+end
+
+function Tag:__newindex(key, value)
+	if key == 'volume' then
+		self._volume = value
+		self:_onChangeVolume()
+	else
+		rawset(self, key, value)
+	end
+end
+
+function ripple.newTag(options)
+	local tag = setmetatable({
+		_tags = {},
+		_children = {},
+	}, Tag)
+	tag.volume = options and options.volume or 1
+	if options and options.tags then
+		tag:tag(unpack(options.tags))
+	end
+	return tag
+end
+
 local Instance = {}
 
+function Instance:_getTotalVolume()
+	local volume = self.volume
+	for tag, _ in pairs(self._tags) do
+		volume = volume * tag:_getTotalVolume()
+	end
+	volume = volume * self._sound:_getTotalVolume()
+	return volume
+end
+
 function Instance:_onChangeVolume()
-	self._source:setVolume(self._volume * self._sound._volume)
+	self._source:setVolume(self:_getTotalVolume())
 end
 
 function Instance:__index(key)
@@ -22,7 +93,39 @@ function Instance:__newindex(key, value)
 	end
 end
 
+function Instance:tag(...)
+	for i = 1, select('#', ...) do
+		local tag = select(i, ...)
+		self._tags[tag] = true
+		tag._children[self] = true
+	end
+	self:_onChangeVolume()
+end
+
+function Instance:untag(...)
+	for i = 1, select('#', ...) do
+		local tag = select(i, ...)
+		self._tags[tag] = nil
+		tag._children[self] = nil
+	end
+	self:_onChangeVolume()
+end
+
 local Sound = {}
+
+function Sound:_onChangeVolume()
+	for _, instance in ipairs(self._instances) do
+		instance:_onChangeVolume()
+	end
+end
+
+function Sound:_getTotalVolume()
+	local volume = self.volume
+	for tag, _ in pairs(self._tags) do
+		volume = volume * tag:_getTotalVolume()
+	end
+	return volume
+end
 
 function Sound:__index(key)
 	if key == 'volume' then
@@ -34,20 +137,39 @@ end
 function Sound:__newindex(key, value)
 	if key == 'volume' then
 		self._volume = value
-		for _, instance in ipairs(self._instances) do
-			instance:_onChangeVolume()
-		end
 	else
 		rawset(self, key, value)
 	end
+end
+
+function Sound:tag(...)
+	for i = 1, select('#', ...) do
+		local tag = select(i, ...)
+		self._tags[tag] = true
+		tag._children[self] = true
+	end
+	self:_onChangeVolume()
+end
+
+function Sound:untag(...)
+	for i = 1, select('#', ...) do
+		local tag = select(i, ...)
+		self._tags[tag] = nil
+		tag._children[self] = nil
+	end
+	self:_onChangeVolume()
 end
 
 function Sound:play(options)
 	local instance = setmetatable({
 		_sound = self,
 		_source = self._source:clone(),
+		_tags = {},
 	}, Instance)
 	instance.volume = options and options.volume or 1
+	if options and options.tags then
+		instance:tag(unpack(options.tags))
+	end
 	instance._source:play()
 	table.insert(self._instances, instance)
 	return instance
@@ -56,6 +178,7 @@ end
 function ripple.newSound(source, options)
 	local sound = setmetatable({
 		_source = source,
+		_tags = {},
 		_instances = {},
 	}, Sound)
 	sound.volume = options and options.volume or 1
